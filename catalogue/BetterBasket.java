068601c689f53ceca243b2ca8f043da1b56b4b52
Lunarixus <Nathan@lunarixus.dev>
2025-01-09 16:06:08 +0000

    src: catalogue/BetterBasket: Add JavaDocs

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -6,28 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Add or get products.
+ * A basket that allows adding, retrieving, and managing products.
+ * Extends the {@link Basket} class and implements {@link Serializable}.
+ * Provides methods to add products, retrieve all products, and clear the basket.
  *
- * @author  Nathan Brooke
+ * @author Nathan Brooke
  * @version 1.0
  */
 public class BetterBasket extends Basket implements Serializable {
   private static final long serialVersionUID = 1L;
+
+  /**
+   * A list to hold the products in the basket.
+   */
   private List<Product> products;
 
+  /**
+   * Constructs an empty {@code BetterBasket}.
+   */
   public BetterBasket() {
     this.products = new ArrayList<>();
   }
 
+  /**
+   * Adds a product to the basket.
+   *
+   * @param product the {@link Product} to add to the basket.
+   */
   public void addProduct(Product product) {
     products.add(product);
-  } // Add a product.
+  }
 
+  /**
+   * Retrieves an unmodifiable list of all products in the basket.
+   *
+   * @return a {@link List} of {@link Product} objects currently in the basket.
+   */
   public List<Product> getProducts() {
     return Collections.unmodifiableList(products);
-  } // Get all products.
+  }
 
+  /**
+   * Clears all products from the basket.
+   * After calling this method, the basket will be empty.
+   */
   public void clear() {
     products.clear();
-  } // Clear all products.
+  }
 }
